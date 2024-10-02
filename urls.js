2a385b9a470d4841d573773c5ea7cec212008cb8
Marine <marinekervella@yahoo.fr>
2024-10-02 15:44:12 +0200

    first commit

---
+++ urls.js
@@ -0,0 +1,12 @@
+// Insert the backend URL given by Vercel in this variable
+const BACKEND_URL = '';
+
+// Insert the frontend URL given by Vercel in this variable
+const FRONTEND_URL = '';
+
+module.exports = { BACKEND_URL, FRONTEND_URL }; // Do not edit/remove this line
+
+
+//https://github.com/Marine35170/mymoviz-backend.git
+
+//https://github.com/Marine35170/mymoviz-frontend.git
