4f2174bb1a3b7d37701b5b4ea5e7bb697f713c5d
Marine <marinekervella@yahoo.fr>
2024-10-02 16:44:57 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TMDB_API_KEY}`)
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             res.json({ movies: data.results });
         });
 });
